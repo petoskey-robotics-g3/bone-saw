1789f5e27227738781349e6fdb8be8b853a7ea1a
Brendon-Egolf <begolf123@gmail.com>
2016-10-06 15:49:58 -0400

    ???

--- FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/internal/opmodes/TankDrive.java
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/internal/opmodes/TankDrive.java
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.robotcontroller.internal.opmodes;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 /**
  * Created by Administrator on 9/29/2016.
@@ -15,12 +16,15 @@ public class TankDrive extends OpMode {
     public void init() {
         leftDrive = hardwareMap.dcMotor.get("left_drive");
         rightDrive = hardwareMap.dcMotor.get("right_drive");
+
+        leftDrive.setDirection(DcMotor.Direction.REVERSE);
     }
 
     @Override
     public void loop() {
-        float leftY = -gamepad1.left_stick_y;
-        float rightY = -gamepad1.right_stick_y;
+        //re-flip these values?
+        float leftY = gamepad1.left_stick_y;
+        float rightY = gamepad1.right_stick_y;
 
         leftDrive.setPower(leftY);
         rightDrive.setPower(rightY);
